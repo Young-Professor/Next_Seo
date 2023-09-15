b3c97ac2a2582f323e80c2e6780c9a4abd7f48bb
Young Professor <youngprofessor991@gmail.com>
2023-09-15 23:41:20 +0300

    -

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ function HomePage() {
     <section>
     <div className="landingpage h-96 bg-gray-500 flex flex-col items-center">
       <h2 className="text-4xl text-white pt-10 font-bold">SELL, BUY, RENT</h2>
-      <h2 className="text-2xl text-white font-bold">CAR SERVING & FLYING MECHANIC</h2>
+      <h2 className="text-2xl text-white font-bold">CAR SERVING & FLYING MECHANICCCCC</h2>
       <button className="bg-blue-500 mt-44 px-8 py-2 rounded-lg shadow-md shadow-gray-400 text-lg">Get Started</button>
     </div>
     <div className="bg-white h-10 w-1/2 mx-auto mt-[-20px] rounded-2xl shadow-xl flex justify-between p-2">
